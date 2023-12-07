f12c11f5df6e652c5a91453acd3d743c3e450951
Tiffany Warren <twarre30@gmail.com>
2023-12-06 23:11:05 -0600

    new ng

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home-page', component: HomePageComponent},
+  {path: '', component: HomePageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
